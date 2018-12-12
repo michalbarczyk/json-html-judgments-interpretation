421d9d4e95fcd70c1b08e225782330bb66880db0
michalbarczyk <barczykmt@gmail.com>
2018-12-12 11:22:00 +0100

    feature names changed

--- src/com/michalbarczyk/judgmentapp/Consts.java
+++ src/com/michalbarczyk/judgmentapp/Consts.java
@@ -3,13 +3,13 @@ package com.michalbarczyk.judgmentapp;
 public class Consts {
 
     public static final String RUBRUM = "rubrum";
-    public static final String JUSTIFICATION = "justif";
-    public static final String JUDGMENTS_NO_PER_JUDGE = "jpj";
-    public static final String TOP_10_JUDGES = "top10j";
-    public static final String STATS_NO_PER_YYYY_MM = "statmonth";
-    public static final String STATS_NO_PER_COURT_TYPE = "stattype";
-    public static final String TOP_10_REGULATIONS = "top10r";
-    public static final String STATS_JUDGES_PER_JUDGMENT = "stat";
+    public static final String JUSTIFICATION = "content";
+    public static final String JUDGMENTS_NO_PER_JUDGE = "judge";
+    public static final String TOP_10_JUDGES = "judges";
+    public static final String STATS_NO_PER_YYYY_MM = "months";
+    public static final String STATS_NO_PER_COURT_TYPE = "courts";
+    public static final String TOP_10_REGULATIONS = "regulations";
+    public static final String STATS_JUDGES_PER_JUDGMENT = "jury";
     public static final String HELP = "help";
     public static final String EXIT = "exit";
 
